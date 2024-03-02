651a078cba17789f8bb532696f94d6c113cd6f1d
jpbarragan <jpbarragann@gmail.com>
2024-03-02 15:40:48 -0500

    Created back-end folder

---
+++ recipes-app-backend/src/test/java/org/recipes/recipesapp/RecipesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.recipes.recipesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
