7dd7249f4a923d2b5af99f86bdd9d99e3796a91f
Jialing Liu <jialingl.liu@gmail.com>
2024-04-13 17:28:51 -0400

    added some styling

--- recipes-app-frontend/src/components/PersonalizedRecipe.js
+++ recipes-app-frontend/src/components/PersonalizedRecipe.js
@@ -1,4 +1,5 @@
 import React from "react";
+//import 'bootstrap/dist/css/bootstrap.css';
 
 //Page display: 
 function PersonalizedRecipe() {
@@ -9,21 +10,21 @@ function PersonalizedRecipe() {
             <h1>My recipes</h1>
             <form>
                 <div>
-                  <label for="recipe-name">My recipe: </label>
+                  <label for="recipe-name"><b>My recipe: </b></label>
                 </div>
                 <div>
                     <input type="text" id="recipe-name" name="name" placeholder="Recipe's name"/>
                 </div>
                 <br></br>
                 <div>
-                    <label for="recipe-description"> Ingredients and instructions:</label>
+                    <label for="recipe-description"><b> Ingredients and instructions: </b></label>
                 </div>
                 <div>
                     <textarea type="text" id="recipe-description" name="description" placeholder="recipe ingredients and instructions"></textarea>
                 </div>
                 <br></br>
                 <div>
-                    <button type="submit">Save my recipe</button>
+                    <button className="btn btn-outline-primary mx-2" style={{backgroundColor: "#CD5C5C"}} type="submit"><b> Save my recipe </b></button>
                 </div>
                 <br></br>
             </form>
